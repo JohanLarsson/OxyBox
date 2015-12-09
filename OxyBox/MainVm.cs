9c2e5ba69be141f11fdfc35347ccf1aea1a3c4bf
Johan Larsson <johan_gavle1@hotmail.com>
2015-12-09 22:57:37 +0100

    Initial

---
+++ OxyBox/MainVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OxyPlot;
+
+namespace OxyBox
+{
+    public class MainVm
+    {
+        public MainVm()
+        {
+            Line1 = Enumerable.Range(0, 360).Select(x => new DataPoint(x, Math.Sin(x * Math.PI / 180))).ToList();
+            Line2 = Enumerable.Range(0, 360).Select(x => new DataPoint(x, Math.Sin((45 + x) * Math.PI / 180))).ToList();
+        }
+
+        public IReadOnlyList<DataPoint> Line2 { get; set; }
+
+        public IReadOnlyList<DataPoint> Line1 { get; set; }
+    }
+}
